5c2ecbd35533a81014ac908ac2c1f264d7522b05
Jorgerodriguezhernandez <78152047+Jorgerodriguezhernandez@users.noreply.github.com>
2021-10-30 13:08:53 +0100

    Fed: Creacion del ejercicio uno y los parametros que usaremos

---
+++ Practica1/src/Ejercicio1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+    public class Ejercicio1 {
+
+        public static void main(String[] args) throws IOException, InterruptedException {
+            Scanner in = new Scanner(System.in);
+            int NumEscrito = in.nextInt();
+            String path = "C:\\Program Files\\Google\\Chrome\\Application\\";
+            Process p = null;
+
+        }
+    }
